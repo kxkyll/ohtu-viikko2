89834374c9299a682b15126d50ebea4b0e43983b
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-03-16 19:32:28 +0200

    t2 riippuvuuksien injektointi

---
+++ viikko2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Reader.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.List;
+
+/**
+ *
+ * @author kxkyllon
+ */
+public interface Reader {
+     List<Player> getPlayers();
+     int extractInt(String str);
+    
+}
